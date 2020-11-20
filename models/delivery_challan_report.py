88605b846d59ed6fcb35f3070b3db5bd811fec4e
root <planet-odoo>
2020-11-20 12:14:39 +0530

    changed page break condition

--- models/delivery_challan_report.py
+++ models/delivery_challan_report.py
@@ -7,6 +7,7 @@ class DeliveryChallan(models.AbstractModel):
     @api.model
     def get_report_values(self, docids, data):
         data = data if data is not None else {}
+        products = data.get('product_values')
         return {
             'datas': data.get('values'),
             'products': data.get('product_values')
